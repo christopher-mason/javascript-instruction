3eaae128da7fc2a64e9c04475cc9cc16b4b454fe
Chris Mason <m1achris43@gmail.com>
2020-12-16 18:37:53 -0500

    edited programs

--- ch01_chessboard.js
+++ ch01_chessboard.js
@@ -1,2 +1,20 @@
-let size = " # " + "# " + "# " + "#"+"\n" + "# " + "# " + "# " + "#";
-console.log(size, "\n" +size,"\n"+ size,"\n"+ size);
+// let size = " # " + "# " + "# " + "#"+"\n" + "# " + "# " + "# " + "#";
+// console.log(size, "\n" +size,"\n"+ size,"\n"+ size);
+
+function makeBoard(size) {
+    let board = "";
+    for (let i = 0; i < size; ++i) {
+      let char = (i % 2 == 0) ? " " : "#";
+     for (let j = 0; j < size; ++j) {
+       board += char;
+        if (char == " ") {
+         char = "#"; 
+        } else {
+         char = " "; 
+        }
+     }
+      board += "\n";
+    }
+     return board;
+   }
+   console.log(makeBoard(8));
--- ch03_beanCounting.js
+++ ch03_beanCounting.js
@@ -1,10 +1,17 @@
-function countBs(str) {
-    let count = 0;
-    for (let i = 0; i < str.length; i++) {
-        if (str.length[i] == "B") {
-            count++;
-        }
-    }
-    return (count);
-}
-console.log(countBs("BBBBC"));
+function countChar(str, char) {
+    let total = 0;
+     for (let i = 0; i < str.length; i++) {
+      if (str[i] == char) {
+        total++;
+      }
+     }
+     return total;
+   }
+   
+   function countBs(str) {
+     return countChar(str, "B");
+   }
+   
+   console.log(countBs("BBC"));
+   
+   console.log(countChar("kakkerlak", "k"));
